857f2e0317298bb893e42398df80d381868a1465
mg3442 <magnusprotas@gmail.com>
2018-09-29 23:31:02 +0300

    complete

--- src/index.js
+++ src/index.js
@@ -1,3 +1,8 @@
 module.exports = function getZerosCount(number) {
-  // your implementation
+  let sum = 0;
+  for(let i = 5 ; i<=number ; i*= 5) {
+	  let x = Math.floor(number/i);
+	  sum += x;
+  }
+	return sum;
 }
